5ad77ca8d63700b6aac7628a7f3dbaaac3b1cf22
moriuz123 <moriuz123@users.noreply.github.com>
2026-09-14 16:59:49 +0700

    chore: cleanup unused temporary, patch, and helper scripts

--- query_sppg.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './src/db/index';
-import { sql } from 'drizzle-orm';
-
-async function main() {
-  const res = await db.execute(sql`SELECT * FROM sppg WHERE nama_sppg ILIKE '%Muara Ciujung Barat 1%'`);
-  console.log(res);
-  process.exit(0);
-}
-main();
